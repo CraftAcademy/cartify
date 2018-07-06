2618ccc1f40433f1798d8d2b7d9268d55de46a6f
Thomas Ochman <thomas@craftacademy.se>
2018-07-06 11:42:42 +0200

    adds respond to format to Cartify::ApplicationController

--- app/controllers/cartify/application_controller.rb
+++ app/controllers/cartify/application_controller.rb
@@ -2,5 +2,7 @@ module Cartify
   class ApplicationController < ::ApplicationController
     protect_from_forgery with: :exception
     include Cartify::Authenticatable
+
+    respond_to :html
   end
 end
