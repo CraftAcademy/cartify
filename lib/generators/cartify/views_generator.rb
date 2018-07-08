45221bbbd47c12db6cdf21ed94c8d69c404e57a5
Thomas Ochman <thomas@craftacademy.se>
2018-07-08 13:23:17 +0200

    WIP adds view templates to use with generator

--- lib/generators/cartify/views_generator.rb
+++ lib/generators/cartify/views_generator.rb
@@ -1,9 +1,32 @@
 module Cartify
     class ViewsGenerator < Rails::Generators::Base
         source_root File.expand_path('../../templates', __FILE__)
+        class_option :scope, type: :string, default: 'all'
 
-        def copy_views
-            template 'login.html.haml', 'app/views/cartify/checkout/login.html.haml'
+        def setup
+            @scope = options['scope']
+            if @scope == 'all'
+                copy_checkout_views
+                copy_checkout_partials
+            elsif @scope == 'checkout_views'
+                copy_checkout_views
+            elsif @scope == 'checkout_partials'
+                copy_checkout_partials
+            else
+              puts 'Please enter a valid scope'
+            end
         end
+
+        private
+
+        def copy_checkout_views
+            template 'checkout/login.html.haml', 'app/views/cartify/checkout/login.html.haml'
+        end
+
+        def copy_checkout_partials
+
+        end
+
+
      end
 end
