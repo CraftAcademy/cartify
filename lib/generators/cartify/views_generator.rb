b0bd75496bea9825177b74159a0788b9d50e02ea
Thomas Ochman <thomas@craftacademy.se>
2018-07-08 20:58:20 +0200

    all relevant templates (related to checkout) can be copied to main application)

--- lib/generators/cartify/views_generator.rb
+++ lib/generators/cartify/views_generator.rb
@@ -21,12 +21,22 @@ module Cartify
 
         def copy_checkout_views
             template 'checkout/login.html.haml', 'app/views/cartify/checkout/login.html.haml'
+            template 'checkout/adresses.html.haml', 'app/views/cartify/checkout/adresses.html.haml'
+            template 'checkout/confirm.html.haml', 'app/views/cartify/checkout/confirm.html.haml'
+            template 'checkout/delivery.html.haml', 'app/views/cartify/checkout/delivery.html.haml'
+            template 'checkout/complete.html.haml', 'app/views/cartify/checkout/complete.html.haml'
         end
 
         def copy_checkout_partials
+            # in views folder
+            template 'checkout/_order_summary.haml', 'app/views/cartify/checkout/_order_summary.html.haml'
+            template 'checkout/_progress.haml', 'app/views/cartify/checkout/_progress.html.haml'
 
+            # in partials folder
+            template 'checkout/partials/_addresses_short.haml', 'app/views/cartify/checkout/partials/_addresses_short.html.haml'
+            template 'checkout/partials/_delivery-lg.haml', 'app/views/cartify/checkout/partials/_delivery-lg.html.haml'
+            template 'checkout/partials/_delivery-xs.haml', 'app/views/cartify/checkout/partials/_delivery-xs.html.haml'
+            template 'checkout/partials/_list_of_items.haml', 'app/views/cartify/checkout/partials/_list_of_items.html.haml'
         end
-
-
      end
 end
