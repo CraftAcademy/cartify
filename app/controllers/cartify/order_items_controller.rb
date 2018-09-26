8e1a0327382395cfe5ecaadf36d6e48756e25929
Thomas <thomas@craftacademy.se>
2018-09-26 15:58:08 +0200

    removes pry :zap:

--- app/controllers/cartify/order_items_controller.rb
+++ app/controllers/cartify/order_items_controller.rb
@@ -4,7 +4,6 @@ module Cartify
   class OrderItemsController < Cartify::ApplicationController
     def create
       current_order.save
-      binding.pry
       @order_item = current_order.order_items.find_or_initialize_by("#{Cartify.product_class.to_s.downcase}_id".to_sym => order_item_params["#{Cartify.product_class.to_s.downcase}_id".to_sym])
       #item = OrderItem.first_or_create(dish_id: order_item_params[:dish_id], order: current_order, quantity: order_item_params[:quantity])
       #@order_item = current_order.order_items.find_or_initialize_by(dish_id: order_item_params[:dish_id])
